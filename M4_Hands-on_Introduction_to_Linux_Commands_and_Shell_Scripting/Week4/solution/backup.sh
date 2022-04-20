07e4d74eeba8b8ffcd892849a7e7f5f46cc9564f
Cesar Robles <cerobles@gmail.com>
2022-04-19 21:00:05 -0500

    Updates

---
+++ M4_Hands-on_Introduction_to_Linux_Commands_and_Shell_Scripting/Week4/solution/backup.sh
@@ -0,0 +1,66 @@
+# This checks if the number of arguments is correct
+# If the number of arguments is incorrect ( $# != 2) print error message and exit
+if [[ $# != 2 ]]
+then
+  echo "backup.sh target_directory_name destination_directory_name"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+# [TASK 1]
+targetDirectory=
+destinationDirectory=
+
+# [TASK 2]
+echo ""
+echo ""
+
+# [TASK 3]
+currentTS=``
+
+# [TASK 4]
+backupFileName=""
+
+# We're going to:
+  # 1: Go into the target directory
+  # 2: Create the backup file
+  # 3: Move the backup file to the destination directory
+
+# To make things easier, we will define some useful variables...
+
+# [TASK 5]
+origAbsPath=``
+
+# [TASK 6]
+cd # <-
+destDirAbsPath=``
+
+# [TASK 7]
+cd # <-
+cd # <-
+
+# [TASK 8]
+yesterdayTS=
+
+declare -a toBackup
+
+for file in $() # [TASK 9]
+do
+  # [TASK 10]
+  if (())
+  then
+    # [TASK 11]
+  fi
+done
+
+# [TASK 12]
+
+# [TASK 13]
+
+# Congratulations! You completed the final project for this course!
